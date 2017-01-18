afa42037b56ac5127434702d770a28380b24203b
Konstantin Semenov <kvsemenoff@mail.ru>
2017-01-18 16:56:41 +0300

    d

--- index.php
+++ index.php
@@ -1,15 +1,12 @@
 ﻿<?php require_once('includes/header.php'); ?>
 <?php require_once('includes/header_top.php'); ?>
-<?php require_once('includes/slider.php'); ?>
-<?php require_once('includes/news.php'); ?>
-<?php require_once('includes/why.php'); ?>
-<?php require_once('includes/gyms.php'); ?>
-<?php require_once('includes/treners.php'); ?>
-<?php require_once('includes/calendar.php'); ?>
-<?php require_once('includes/we_grou_up.php'); ?>
-<?php require_once('includes/zapis.php'); ?>
-<?php require_once('includes/about.php'); ?>
-<?php require_once('includes/logos.php'); ?>
+<?php require_once('includes/portfolio.php'); ?>
+<?php require_once('includes/my_aproach.php'); ?>
+<?php require_once('includes/advantages.php'); ?>
+<?php require_once('includes/price.php'); ?>
+<?php require_once('includes/scheme.php'); ?>
+<?php require_once('includes/alsow.php'); ?>
+<?php require_once('includes/contacts.php'); ?>
 <?php require_once('includes/footer_bottom.php'); ?>
 <?php require_once('includes/footer.php'); ?>
 
