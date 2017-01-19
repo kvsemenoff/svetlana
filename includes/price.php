f2a1a549f5128f646500f1cece2673a3ff54020b
engine77 <zaur.ka.00@mail.ru>
2017-01-19 13:30:16 +0300

    19

--- includes/price.php
+++ includes/price.php
@@ -1,2 +1,26 @@
-</body>
-</html>
+<section class="section section_padding">
+	<div class="container">
+		<h1>Pricing Table</h1>
+		<h2>Стоимость реализации ваших идей</h2>
+		<div class="df-price df-price_view">
+			<div class="df-price__bg-box df-price__bg-box_view">
+				<span class="df-price__t1 df-price__t1_view">Дизайн-макет</span>
+				<span class="df-price__t2 df-price__t2_view">13 500 Р–</span>
+				<span class="df-price__t3 df-price__t3_view">Разработка дизайн-макета</span>
+			</div>
+			<div class="df-price__textblock df-price__textblock_view">
+				Индивидуальный дизайн<br>
+				Корректировка текстов<br>
+				Разработка прототипа<br>
+				Составление мудборда<br>
+				Качественная графика<br>
+				<div class="df-price__boxmiddle df-price__boxmiddle_view">
+					<span>На выходе вы получите продающий PSD макет 
+					лендинга</span> <br>
+					<input type="submit" value="Заказать" class="df-price__input df-price__input_view">
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
