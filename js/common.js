56411a56340976633e13d64a9e217c79624e7900
Almaz Dzhanaliev <aldjanaliev@gmail.com>
2017-01-22 00:21:10 +0600

    complete portfolio

--- js/common.js
+++ js/common.js
@@ -34,6 +34,35 @@ $(document).ready(function(){
         $(this).toggleClass('az-select-focus');
     });
 
+    $('.portfolio__slider').slick({
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      adaptiveHeight: true,
+      // autoplay: true,
+      autoplaySpeed: 5000
+    });
+
+    $('.has-tooltip').mouseover(function(){
+
+        var tooltip_txt = $(this).attr('data-tooltip');
+        var pos = $(this).offset();
+
+        $(this).append('<div class="tooltip">' + tooltip_txt + '</div>')
+        $('.tooltip').css({
+            'top': pos.top + 20 + 'px',
+            'left': pos.top + 42 + 'px'
+        })
+        $('.has-tooltip').mouseout(function(){
+            $('.tooltip').remove();
+        });
+
+        $('span > .tooltip').css({
+            'top': pos.top + 22 + 'px',
+            'left': pos.top + 2 + 'px'
+        })
+    });
+
  });
 
 
