b538b9f14e71c2a786ae518b34aa559d67078ca7
Almaz Dzhanaliev <aldjanaliev@gmail.com>
2017-01-21 21:11:39 +0600

    my_apploach is ready

---
+++ includes/portfolio.php
@@ -0,0 +1,22 @@
+<section class="section section_padding section_portfolio">
+    <div class="contacts__title">
+        <h2 class="contacts__title1">
+            Portfolio 
+        </h2>
+        <h2 class="contacts__title2">
+            Портфолио
+        </h2>
+    </div>
+    <div class="portfolio__inner clearfix">
+        <div class="portfolio__slider">
+            <div class="slide">
+                <div class="slide__img">
+                    <img class="slide__img-photo" alt="" src="">
+                </div>
+                <div class="slide__text">
+                    <span class="text__descr">Landing Page</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>  
