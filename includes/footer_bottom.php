8047061174b43b9b70baabc192881c52ff226cea
unknown <anzarsh@mail.ru>
2017-01-23 10:45:00 +0300

    add 1

--- includes/footer_bottom.php
+++ includes/footer_bottom.php
@@ -1,32 +1,33 @@
+<div class="clearfix"></div>
 <section class="section section_padding section_padding-db-view">
 	<div class="container">
 		<div class="row">
 			<div class="col-md-3">
-				<p>
-					<span>Svezhaya landing</span><br>
-					Веб-дизайнер Светлана Свежая
-				</p>
+				<p class="db-footer-left">
+			<img src="img/db-footer-left.png" alt="img" class="img-responsive">
 			</div>
 			<div class="col-md-3">
 				<p>
 					&copy;2017 Все права защищены<br>
-					Политика конфиденциальности<br>
-					Скачать презентацию
+					Политика конфиденциальности <br>
+					<a href="#">Скачать презентацию</a>
+				
 				</p>
 			</div>
 			<div class="col-md-3">
 				<p>
 					ИП Гусева И. А. <br>
-					Инн 526007734174 <br>
-					ОгрнИП 316527500009481
+					ИНН 526007734174 <br>
+					ОГРНИП 316527500009481
 
 				</p>
 			</div>
 			<div class="col-md-3">
-				<p>
+				<p class="db-footer-right">
 					<span>+7-910-394-46-64</span><br>
 					Заказать звонок<br>
-					Скачать бриф
+					<a href="#">Скачать бриф</a>
+				
 				</p>
 			</div>
 		</div>
