e93d89858e5f6add7531403f5014c804cc728051
Berzeg07 <berezg86@gmail.com>
2017-01-23 10:31:24 +0300

    29.14

--- js/common.js
+++ js/common.js
@@ -2,38 +2,13 @@
 
 $(document).ready(function(){
 
-    $('.az-select').each(function(){
-        var select = $(this);    
-        var option = select.find('select option');
-        var str = '<div class="az-options">';
-        select.find('option').each(function(){
-            str+= '<div data-val="' +$(this).val() + '">' + $(this).text() + '</div>'
-        });
-        str+= '</div>';
-        select.html(select.html() + str);
-
-        select.find('select').mousedown(function(){
-            return false;
-        });
-        select.mouseup(function(){
-            select.find('select').focus();
-        });
-        select.find('.az-options div[data-val]').click(function(){
-            select.find('select').val($(this).attr('data-val'));
-        });
-        select.find('select').focusout(function(){
-            if(!select.is(':hover')){
-                select.find('.az-options').slideUp(0);
-                select.removeClass('az-select-focus');
-            }
-        });
+    $('.dd-ico').click(function(){
+        $('.dd-min-menu-wrap').show(500);
+    });
+    $('.dd-close').click(function(){
+        $('.dd-min-menu-wrap').hide(500);
     });
 
-    $(".az-select").click(function(){
-        $(this).find('.az-options').slideToggle(0);
-        $(this).toggleClass('az-select-focus');
-    });
-
- });
+});
 
 
