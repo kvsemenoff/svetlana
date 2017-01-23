063d5c605705872d9ad205053684bd3a3a706d71
Berzeg07 <berezg86@gmail.com>
2017-01-23 14:21:07 +0300

    29.14

--- includes/header_top.php
+++ includes/header_top.php
@@ -1,5 +1,5 @@
 <section class="dd-top-header">
-	
+	<div class="container pl-none">
 		<!-- <div class="dd-element dd-logo">
 			<img class="img-responsive" src="img/logo.png" alt="">
 		</div> -->
@@ -9,15 +9,17 @@
 		<div class="dd-element dd-element_style dd-element-padding">
 			<span>Работаю по всей России и странам СНГ</span>
 		</div>
-		
-
-	<div class="dd-menu-element">
 		<div class="dd-element dd-tel-style ml-tel">
 			<span>+7-910-394-46-64</span>
 		</div>
-		<div class="dd-element dd-element_style dd-dis-none">
+		
+	</div>
+
+		<div class="dd-menu-element">
+
+		<!-- <div class="dd-element dd-element_style dd-dis-none">
 			<span>Меню</span>
-		</div>
+		</div> -->
 		<div class="dd-ico">
 			<img src="img/menu-b.png" alt="">
 		</div>
@@ -25,37 +27,38 @@
 			<img src="img/menu-w.png" alt="">
 		</div>
 	</div>
-	<div class="dd-min-menu-wrap">
-		<div class="dd-close">
-			<div class="dd-position-close">
-				<img src="img/close.png" alt="">
-			</div>
+
+<div class="dd-min-menu-wrap">
+	<div class="dd-close">
+		<div class="dd-position-close">
+			<img src="img/close.png" alt="">
 		</div>
-		<ul>
-			<li><a href="#">01<br> Главная</a></li>
-			<li><a href="#">02<br> Портфолио</a></li>
-			<li><a href="#">03<br> Обо мне</a></li>
-			<li><a href="#">04<br> Преимущества</a></li>
-			<li><a href="#">05<br> Стоимость</a></li>
-			<li><a href="#">06<br> Этапы и сроки</a></li>
-			<li><a href="#">07<br> контакты</a></li>
-		</ul>
 	</div>
-	<div class="dd-contnet-header">
-		<div class="dd-content-header-txt dd-z-index">
-			
-				<span>Современные Landing Page</span><br>
-				<span>Сильный дизайн. Приемлемая цена. Точно в срок.</span>
+	<ul>
+		<li><a href="#">01<br> Главная</a></li>
+		<li><a href="#">02<br> Портфолио</a></li>
+		<li><a href="#">03<br> Обо мне</a></li>
+		<li><a href="#">04<br> Преимущества</a></li>
+		<li><a href="#">05<br> Стоимость</a></li>
+		<li><a href="#">06<br> Этапы и сроки</a></li>
+		<li><a href="#">07<br> контакты</a></li>
+	</ul>
+</div>
+<div class="dd-contnet-header">
+	<div class="dd-content-header-txt dd-z-index">
+
+		<span>Современные Landing Page</span><br>
+		<span>Сильный дизайн. Приемлемая цена. Точно в срок.</span>
 		
-			
-		</div>
-		<div class="dd-f1-wrap"></div>
-		<div class="dd-header-button ">
-			<a class="active" href="#">Портфолио</a>
-		</div>
-		<div class="dd-header-button dd-header-button2">
-			<a href="#">Заказать звонок</a>
-		</div>
+
 	</div>
+	<div class="dd-f1-wrap"></div>
+	<div class="dd-header-button ">
+		<a class="active" href="#">Портфолио</a>
+	</div>
+	<div class="dd-header-button dd-header-button2">
+		<a href="#">Заказать звонок</a>
+	</div>
+</div>
 </section>
 
