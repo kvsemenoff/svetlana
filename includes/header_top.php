1a876c4368c6e026a0146fa4f0ae2d60f4954268
Berzeg07 <berezg86@gmail.com>
2017-01-23 13:27:21 +0300

    29.14

--- includes/header_top.php
+++ includes/header_top.php
@@ -1,22 +1,27 @@
 <section class="dd-top-header">
-	<div class="dd-element dd-logo">
-		<img class="img-responsive" src="img/logo.png" alt="">
-	</div>
-	<div class="dd-element dd-element_style dd-element-padding-2 dd-padding-top">
-		<span>Веб-дизайнер Светлана Свежая</span>
-	</div>
-	<div class="dd-element dd-element_style dd-element-padding dd-padding-top">
-		<span>Работаю по всей России и странам СНГ</span>
-	</div>
-	<div class="dd-element dd-tel-style dd-padding-top ml-tel">
-		<span>+7-910-394-46-64</span>
+	<div class="dd-all-none">
+		<div class="dd-element dd-logo">
+			<img class="img-responsive" src="img/logo.png" alt="">
+		</div>
+		<div class="dd-element dd-element_style dd-element-padding-2 dd-padding-top">
+			<span>Веб-дизайнер Светлана Свежая</span>
+		</div>
+		<div class="dd-element dd-element_style dd-element-padding dd-padding-top">
+			<span>Работаю по всей России и странам СНГ</span>
+		</div>
+		<div class="dd-element dd-tel-style dd-padding-top ml-tel">
+			<span>+7-910-394-46-64</span>
+		</div>
 	</div>
 	<div class="dd-menu-element dd-padding-top">
-		<div class="dd-element dd-element_style dd-dis-none">
+		<div class="dd-element dd-element_style dd-dis-none dd-all-none">
 			<span>Меню</span>
 		</div>
 		<div class="dd-ico">
 			<img src="img/menu-b.png" alt="">
+		</div>
+		<div class="dd-ico2">
+			<img src="img/menu-w.png" alt="">
 		</div>
 	</div>
 	<div class="dd-min-menu-wrap">
@@ -36,12 +41,23 @@
 		</ul>
 	</div>
 	<div class="dd-contnet-header">
-		<div class="dd-content-header-txt">
-			<span>Современные Landing Page</span><br>
-			<span>Сильный дизайн. Приемлемая цена. Точно в срок.</span>
+		<div class="dd-content-header-txt dd-z-index">
+			
+				<span>Современные Landing Page</span><br>
+				<span>Сильный дизайн. Приемлемая цена. Точно в срок.</span>
+		
+			
 		</div>
-		<a href="#"></a>
-		<a href="#"></a>
+		<div class="dd-f1-wrap"></div>
+		<div class="dd-header-button ">
+			<a class="active" href="#">Портфолио</a>
+		</div>
+		<div class="dd-header-button dd-header-button2">
+			<a href="#">Заказать звонок</a>
+		</div>
+	</div>
+	<div class="dd-f2-wrap">
+		<img src="img/f2.png" alt="">
 	</div>
 </section>
 
--- js/common.js
+++ js/common.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function(){
 
-    $('.dd-ico').click(function(){
+    $('.dd-ico, .dd-ico2').click(function(){
         $('.dd-min-menu-wrap').show(500);
     });
     $('.dd-close').click(function(){
