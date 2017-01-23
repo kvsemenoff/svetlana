cf90a9734a10bc7f17aaf6dbd80a28c9bba0796d
engine77 <zaur.ka.00@mail.ru>
2017-01-23 11:46:20 +0300

    20

--- includes/price.php
+++ includes/price.php
@@ -2,72 +2,74 @@
 	<div class="df-container">
 		<h2 class="eng">Pricing Table</h2>
 		<h2>Стоимость реализации ваших идей</h2>
-		<div class="df-price df-price_view">
-			<div class="df-price__bg-box df-price__bg-box_view">
-				<span class="df-price__t1 df-price__t1_view">Дизайн-макет</span>
-				<span class="df-price__t2 df-price__t2_view">13 500 Р–</span>
-				<span class="df-price__t3 df-price__t3_view">Разработка дизайн-макета</span>
+		<div>
+			<div class="df-price df-price_view">
+				<div class="df-price__bg-box df-price__bg-box_view">
+					<span class="df-price__t1 df-price__t1_view">Дизайн-макет</span>
+					<span class="df-price__t2 df-price__t2_view">13 500 Р–</span>
+					<span class="df-price__t3 df-price__t3_view">Разработка дизайн-макета</span>
+				</div>
+				<div class="df-price__textblock df-price__textblock_view">
+					Индивидуальный дизайн<br>
+					Корректировка текстов<br>
+					Разработка прототипа<br>
+					Составление мудборда<br>
+					Качественная графика<br>
+					<div class="df-price__boxmiddle df-price__boxmiddle_view">
+						<span>На выходе вы получите продающий PSD макет 
+						лендинга</span> <br>
+						<input type="submit" value="Заказать" class="df-price__input df-price__input_view">
+					</div>
+				</div>
 			</div>
-			<div class="df-price__textblock df-price__textblock_view">
-				Индивидуальный дизайн<br>
-				Корректировка текстов<br>
-				Разработка прототипа<br>
-				Составление мудборда<br>
-				Качественная графика<br>
-				<div class="df-price__boxmiddle df-price__boxmiddle_view">
-					<span>На выходе вы получите продающий PSD макет 
-					лендинга</span> <br>
-					<input type="submit" value="Заказать" class="df-price__input df-price__input_view">
+			<div class="df-price df-price_view">
+				<div class="df-price__bg-box df-price__bg-box_view df-bg2">
+					<span class="df-price__t1 df-price__t1_view">Стандарт</span>
+					<span class="df-price__t2 df-price__t2_view">17 500 Р–</span>
+					<span class="df-price__t3 df-price__t3_view">Разработка landing Page</span>
+				</div>
+				<div class="df-price__textblock df-price__textblock_view">
+					Индивидуальный дизайн<br>
+					Корректировка текстов<br>
+					Разработка прототипа<br>
+					Составление мудборда<br>
+					Качественная графика<br>
+					Верстка<br>
+					Кроссбраузерность<br>
+					Настройка форм (заявок) <br>
+					Установка сайта на хостинг
+					<div class="df-price__boxmiddle df-price__boxmiddle_view">
+						<span>На выходе вы получите продающий сайт</span> <br>
+						<input type="submit" value="Заказать" class="df-price__input df-price__input_view">
+					</div>
+				</div>
+			</div>
+			<div class="df-price df-price_view">
+				<div class="df-price__bg-box df-price__bg-box_view">
+					<span class="df-price__t1 df-price__t1_view">Расширенный</span>
+					<span class="df-price__t2 df-price__t2_view">22 300 Р–</span>
+					<span class="df-price__t3 df-price__t3_view">Разработка landing Page</span>
+				</div>
+				<div class="df-price__textblock df-price__textblock_view">
+					Индивидуальный дизайн<br>
+					Корректировка текстов<br>
+					Разработка прототипа<br>
+					Составление мудборда<br>
+					Качественная графика<br>
+					Верстка<br>
+					Кроссбраузерность<br>
+					Настройка форм (заявок) <br>
+					Установка сайта на <br> хостинг
+					Анимация и parallax эффекты
+					<div class="df-price__boxmiddle df-price__boxmiddle_view">
+						<span>На выходе вы получите продающий сайт с анимацией
+						и эффектами</span> <br>
+						<input type="submit" value="Заказать" class="df-price__input df-price__input_view">
+					</div>
 				</div>
 			</div>
 		</div>
-		<div class="df-price df-price_view">
-			<div class="df-price__bg-box df-price__bg-box_view df-bg2">
-				<span class="df-price__t1 df-price__t1_view">Стандарт</span>
-				<span class="df-price__t2 df-price__t2_view">17 500 Р–</span>
-				<span class="df-price__t3 df-price__t3_view">Разработка landing Page</span>
-			</div>
-			<div class="df-price__textblock df-price__textblock_view">
-				Индивидуальный дизайн<br>
-				Корректировка текстов<br>
-				Разработка прототипа<br>
-				Составление мудборда<br>
-				Качественная графика<br>
-				Верстка<br>
-				Кроссбраузерность<br>
-				Настройка форм (заявок) <br>
-				Установка сайта на хостинг
-				<div class="df-price__boxmiddle df-price__boxmiddle_view">
-					<span>На выходе вы получите продающий сайт</span> <br>
-					<input type="submit" value="Заказать" class="df-price__input df-price__input_view">
-				</div>
-			</div>
-		</div>
-		<div class="df-price df-price_view">
-			<div class="df-price__bg-box df-price__bg-box_view">
-				<span class="df-price__t1 df-price__t1_view">Расширенный</span>
-				<span class="df-price__t2 df-price__t2_view">22 300 Р–</span>
-				<span class="df-price__t3 df-price__t3_view">Разработка landing Page</span>
-			</div>
-			<div class="df-price__textblock df-price__textblock_view">
-				Индивидуальный дизайн<br>
-				Корректировка текстов<br>
-				Разработка прототипа<br>
-				Составление мудборда<br>
-				Качественная графика<br>
-				Верстка<br>
-				Кроссбраузерность<br>
-				Настройка форм (заявок) <br>
-				Установка сайта на <br> хостинг
-				Анимация и parallax эффекты
-				<div class="df-price__boxmiddle df-price__boxmiddle_view">
-					<span>На выходе вы получите продающий сайт с анимацией
-					и эффектами</span> <br>
-					<input type="submit" value="Заказать" class="df-price__input df-price__input_view">
-				</div>
-			</div>
-		</div>
-		<div class="df-price__text-block">
+		<div class="df-price__text-block df-price__text-block_view">
 			<h4>Сопутствующие услуги:</h4>
 			<span>Копирайтинг (написание текста для сайта) – 2 000 р.</span>
 			<span>Установка сайта на систему управления WordPress – от 4 000 р.</span>
